080616d5b8334b6d42c201eddaa5bd878849edd4
Tom Southwick <thomas.h.southwick@gmail.com>
2022-01-27 11:25:37 +1100

    Trying to resolve comment created date issues

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -5,29 +5,29 @@ const commentData = [
     content: "Blossoming Apricot",
     user_id: 1,
     blog_id: 2,
-    created_date: "23/06/2024",
+    created_date: "230621",
   },
-  {
-    content: "Sam has COVID19",
-    user_id: 1,
-    blog_id: 1,
-    created_date: "23/06/2024",
-  },
-  {
-    content: "Blossoming Hello",
-    user_id: 1,
-    blog_id: 3,
-    created_date: "23/06/2024",
-  },
+  // {
+  //   content: "Sam has COVID19",
+  //   user_id: 1,
+  //   blog_id: 1,
+  //   created_date: "23/06/2024",
+  // },
+  // {
+  //   content: "Blossoming Hello",
+  //   user_id: 1,
+  //   blog_id: 3,
+  //   created_date: "23/06/2024",
+  // },
 ];
 
-// const seedComment = async () => await Comment.bulkCreate(commentData);
+const seedComment = async () => await Comment.bulkCreate(commentData);
 
-const seedComment = async () => {
-  for (let index = 0; index < commentData.length; index++) {
-    const comment = commentData[index];
-    await Comment.create(comment);
-  }
-};
+// const seedComment = async () => {
+//   for (let index = 0; index < commentData.length; index++) {
+//     const comment = commentData[index];
+//     await Comment.create(comment);
+//   }
+// };
 
 module.exports = seedComment;
