32df11c830a4c5edb3da4cf6f9432aa1b551493d
Tom Southwick <thomas.h.southwick@gmail.com>
2022-01-27 11:02:33 +1100

    updating comment models

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -5,19 +5,29 @@ const commentData = [
     content: "Blossoming Apricot",
     user_id: 1,
     blog_id: 2,
+    created_date: "23/06/2024",
   },
   {
     content: "Sam has COVID19",
     user_id: 1,
     blog_id: 1,
+    created_date: "23/06/2024",
   },
   {
     content: "Blossoming Hello",
     user_id: 1,
     blog_id: 3,
+    created_date: "23/06/2024",
   },
 ];
 
-const seedComments = async () => await Comment.bulkCreate(commentData);
+// const seedComment = async () => await Comment.bulkCreate(commentData);
 
-module.exports = seedComments;
+const seedComment = async () => {
+  for (let index = 0; index < commentData.length; index++) {
+    const comment = commentData[index];
+    await Comment.create(comment);
+  }
+};
+
+module.exports = seedComment;
